04ae94ef5efaf10d53095efb0596958f84c0b289
Laurie Boyes <laurie.boyes@madetech.com>
2021-07-03 13:34:34 +0100

    formatting

--- html-stuff.spec.js
+++ html-stuff.spec.js
@@ -1,63 +1,63 @@
 const { leagueTableToHtml } = require('./html-stuff');
 
 describe('html stuff', () => {
-     it('should return the league table view', () => {
-        
-        const leagueTable = [
-            {
-              "name": "Laurie",
-              "count": 3,
-              "emojiCounts": {
-                ":boom:": 2,
-                ":what:": 3,
-              },
-              "tgifs": [],
-              "favouriteEmoji": {
-                "count": 3,
-                "emoji": ":what:",
-              },
-              "rank": 3
-            },
-            {
-              "name": "Freya",
-              "count": 5,
-              "emojiCounts": {
-                ":boom:": 2
-              },
-              "favouriteEmoji": {
-                "count": 2,
-                "emoji": ":boom:",
-              },
-              "tgifs": [],
-              "rank": 1
-            },
-            {
-              "name": "Scott",
-              "count": 5,
-              "emojiCounts": {
-                ":boom:": 1
-              },
-              "tgifs": [],
-              "rank": 1
-            },
-            {
-              "name": "Bill",
-              "count": 4,
-              "emojiCounts": {
-                ":boom:": 2
-              },
-              "favouriteEmoji": {
-                "count": 2,
-                "emoji": ":boom:",
-              },
-              "tgifs": [],
-              "rank": 2
-            }
-          ];
+  it('should return the league table view', () => {
 
-          // expect no errors
-          const result = leagueTableToHtml(leagueTable);
-          console.log(result);
-          console.log('hello');
-    });
+    const leagueTable = [
+      {
+        "name": "Laurie",
+        "count": 3,
+        "emojiCounts": {
+          ":boom:": 2,
+          ":what:": 3,
+        },
+        "tgifs": [],
+        "favouriteEmoji": {
+          "count": 3,
+          "emoji": ":what:",
+        },
+        "rank": 3
+      },
+      {
+        "name": "Freya",
+        "count": 5,
+        "emojiCounts": {
+          ":boom:": 2
+        },
+        "favouriteEmoji": {
+          "count": 2,
+          "emoji": ":boom:",
+        },
+        "tgifs": [],
+        "rank": 1
+      },
+      {
+        "name": "Scott",
+        "count": 5,
+        "emojiCounts": {
+          ":boom:": 1
+        },
+        "tgifs": [],
+        "rank": 1
+      },
+      {
+        "name": "Bill",
+        "count": 4,
+        "emojiCounts": {
+          ":boom:": 2
+        },
+        "favouriteEmoji": {
+          "count": 2,
+          "emoji": ":boom:",
+        },
+        "tgifs": [],
+        "rank": 2
+      }
+    ];
+
+    // expect no errors
+    const result = leagueTableToHtml(leagueTable);
+    console.log(result);
+    console.log('hello');
+  });
 });
